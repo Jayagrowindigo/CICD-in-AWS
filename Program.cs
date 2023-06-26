256d1376b9decf07ccaa5fc229429a489d549433
Jayagrowindigo <137497947+Jayagrowindigo@users.noreply.github.com>
2023-06-26 06:06:13 +0530

    Code added for CICD test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("CICD on AWS programme");
+Process process = new Process();
+process.StartInfo.UseShellExecute = true;
+process.StartInfo.FileName = "chrome";
+//process.StartInfo.Arguments = @"http://www.stackoverflow.net/";
+//process.StartInfo.Arguments = @"https://web.whatsapp.com/";
+//process.StartInfo.Arguments = @"https://web.whatsapp.com/";
+//process.StartInfo.Arguments = @"https://outlook.office.com/mail/";
+process.StartInfo.Arguments = @"https://www.flipkart.com/";
+//process.StartInfo.Arguments = @"https://web.whatsapp.com/";
+
+process.Start();
